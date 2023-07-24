46fe6a5b7b6bd50174b258c560928de9193844d6
Raufur Rahman <raufur.rahman@brilliant.com.bd>
2023-07-24 12:48:33 +0600

    test android built finale

--- app/src/main/java/com/example/mytextview/MainActivity.java
+++ app/src/main/java/com/example/mytextview/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.mytextview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTextView = findViewById(R.id.life_story);
+
+        nameTextView.setText("Lets Begin");
     }
 }
